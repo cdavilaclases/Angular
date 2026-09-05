7d48f5bf19ac05515666e6435cdb646bf88bf436
cdavilaclases <ing.carlosdav26@gmail.com>
2026-09-05 10:40:33 -0600

    Hola mundo A

---
+++ holaMundoA/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  imports: [RouterOutlet,FormsModule],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('holaMundoA');
+  saludar:string = "mundo";
+}
